d8cc365273f35ca74d6b041c946f21883ee263e8
asliddin <asliddintojaliyev88@gmail.com>
2022-12-10 13:37:25 +0500

    as-dev

--- inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -1,8 +1,12 @@
 package com.example.inventoryservice;
 
+import com.example.inventoryservice.model.Inventory;
+import com.example.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,7 +16,7 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
-/*	@Bean
+	@Bean
 	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
 		return args -> {
 			Inventory inventory1=new Inventory();
@@ -26,5 +30,5 @@ public class InventoryServiceApplication {
 			inventoryRepository.save(inventory1);
 			inventoryRepository.save(inventory2);
 		};
-	}*/
+	}
 }
